208dd7a91beda637fb8f2b364d8258cf6be3512f
Philippe Lopes <philippe.lopes@live.fr>
2024-01-06 08:03:28 +0900

    Country is no longer a string but an element from iso_country; added a struct wrapper to save/load it to database

--- src-tauri/src/app/travel.rs
+++ src-tauri/src/app/travel.rs
@@ -1,50 +1,57 @@
+use isocountry::CountryCode;
 use serde::Serialize;
 use sqlx::FromRow;
+use crate::app::country_iso_wrapper::CountryIsoWrapper;
 
 use crate::db;
 
 #[derive(Debug, Clone, FromRow, Serialize)]
 pub struct Travel {
     rowid: i64,
-    country: String,
+    country: CountryIsoWrapper,
 }
 
 // Get all travels
 #[tauri::command]
-pub(crate) async fn get_travels(
+pub async fn get_travels(
     conn: tauri::State<'_, db::DbConnection>
 ) -> Result<Vec<Travel>, String> {
     // Lock mutex
     let conn = conn.db.lock().await;
 
     // Perform query
-    let all_travels = sqlx::query_as::<_, Travel>("\
-        SELECT ROWID, * \
-        FROM travel \
-    ",
+    let all_travels = sqlx::query_as::<_, Travel>(r#"
+        SELECT ROWID, *
+        FROM travel
+    "#,
     )
         .fetch_all(&*conn)
         .await
         .map_err(|e| e.to_string());
-    all_travels
+    dbg!(all_travels)
 }
 
 // Create one travel
 #[tauri::command]
-pub(crate) async fn create_travel(
+pub async fn create_travel(
     conn: tauri::State<'_, db::DbConnection>,
     country: String,
 ) -> Result<Travel, String> {
     // Lock mutex
     let conn = conn.db.lock().await;
 
+    // Create country object
+    let country_wrapper: CountryIsoWrapper = CountryCode::for_alpha3(&*country)
+        .map_err(|e| e.to_string())?
+        .into();
+
     // Perform query
-    let travel_created = sqlx::query_as::<_, Travel>("\
-        INSERT INTO travel (country)\
-        VALUES(?) \
+    let travel_created = sqlx::query_as::<_, Travel>("
+        INSERT INTO travel (country)
+        VALUES (?)
         RETURNING ROWID, *
     ")
-        .bind(country)
+        .bind(country_wrapper)
         .fetch_one(&*conn)
         .await
         .map_err(|e| e.to_string());
