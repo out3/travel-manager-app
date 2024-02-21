21a8f03e8281d81f9ab7243edb84ea7b3caac4f7
Philippe Lopes <philippe.lopes@live.fr>
2024-02-21 19:15:56 +0900

    Updated TravelInfoSkeleton.tsx

--- src/components/Travel/TravelInfoSkeleton.tsx
+++ src/components/Travel/TravelInfoSkeleton.tsx
@@ -8,51 +8,36 @@ import {PaperPlaneIcon} from "@radix-ui/react-icons";
 import {Skeleton} from "@/components/ui/skeleton.tsx";
 
 
-function TravelInfo() {
-    function displayProgressBar(): JSX.Element {
-        return <Progress value={100} indicatorClassName="bg-secondary"/>;
-    }
-
-    function displayDates(): JSX.Element {
-        const output = [];
-        // Add the start date if it exists
-        output.push(
-            <div className="flex justify-start mb-2">
-                <PaperPlaneIcon className="-rotate-45 h-4 w-4 self-center mr-2"/>
-                <Skeleton className="h-4 w-[250px]"/>
-            </div>
-        )
-        // Add the progress bar anyway
-        output.push(displayProgressBar())
-        // Add the end date if it exists
-        output.push(
-            <div className="flex justify-end mt-2">
-                <Skeleton className="h-4 w-[250px]"/>
-                <PaperPlaneIcon className="rotate-45 h-4 w-4 self-end ml-2"/>
-            </div>
-        )
-        return <>{output}</>;
-    }
-
+function TravelInfoSkeleton() {
     return (
         <>
             <Card className="w-[300px]">
                 <CardHeader>
                     <CardTitle>
-                        <Skeleton className="h-4 w-[250px]"/>
+                        <Skeleton className="h-4 w-[200px]"/>
                     </CardTitle>
                     <CardDescription>
-                        <Skeleton className="h-4 w-[250px]"/>
-                        (<Skeleton className="h-4 w-[250px]"/>)
+                        <Skeleton className="h-4 w-[150px]"/>
                     </CardDescription>
                     <Separator/>
                 </CardHeader>
                 <CardContent>
-                    {displayDates()}
+                    {/* Start date */}
+                    <div className="flex justify-start mb-2">
+                        <PaperPlaneIcon className="-rotate-45 h-4 w-4 self-center mr-2"/>
+                        <Skeleton className="h-4 w-[150px]"/>
+                    </div>
+                    {/* Progress bar */}
+                    <Progress value={100} indicatorClassName="bg-secondary"/>
+                    {/* End date */}
+                    <div className="flex justify-end mt-2">
+                        <Skeleton className="h-4 w-[150px]"/>
+                        <PaperPlaneIcon className="rotate-45 h-4 w-4 self-end ml-2"/>
+                    </div>
                 </CardContent>
             </Card>
         </>
     )
 }
 
-export default TravelInfo
+export default TravelInfoSkeleton
